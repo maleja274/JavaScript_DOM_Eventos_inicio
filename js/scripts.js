c80288aa5170a83b061f9400f954bf7e9a2c74ae
maleja274 <maleja274@hotmail.com>
2022-09-06 21:50:17 -0500

    validacion-formulario.js

--- js/scripts.js
+++ js/scripts.js
@@ -37,22 +37,108 @@ console.log(nuevoEnlace);
 
 // eventos
 
-console.log(1);
+// console.log(1);
 
-window.addEventListener('load', function() { // load espera a que el JS y los archivos que dependendel HTMLesten listos
-    console.log(2);
+// window.addEventListener('load', function() { // load espera a que el JS y los archivos que dependendel HTMLesten listos
+//     console.log(2);
+// });
+
+// window.onload =function () {
+//     console.log(3);
+// }
+
+// document.addEventListener('DOMContentLoaded', function () { // solo espera el hmtl, pero no espera css ni imagenes
+//     console.log(4);
+// })
+
+// console.log(5);
+
+// window.onscroll = function (evento) {
+//     console.log(evento);
+// }
+
+// // seleccionar elementos y asociarles un evento
+
+// const btnEnviar = document.querySelector('.boton--primario');
+// btnEnviar.addEventListener('click', function(evento) {
+//     console.log(evento);
+//     evento.preventDefault();
+
+
+//     // validar formulario 
+//     console.log('enviando formulario');
+// });
+
+
+//eventos de los input y los texarea
+
+const datos = {
+    nombre: '',
+    email: '',
+    mensaje: ''
+}
+
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+const formulario = document.querySelector('.formulario');
+
+nombre.addEventListener('input', leerTexto);
+email.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+// El evento de submit
+formulario.addEventListener('submit', function(evento) {
+    evento.preventDefault();
+
+    // validar el formulario
+
+    const {nombre, email, mensaje } = datos;
+
+    if(nombre === '' || email === '' || mensaje === '') {
+        mostrarError('todos los campos son obligatorios');
+       
+        return; // corta la ejecucion del codigo
+    }
+  
+    // crear alerta de envio correcto
+    mostrarMensaje('Mensaje enviado correctamente');
 });
 
-window.onload =function () {
-    console.log(3);
+function leerTexto(e) {
+    // console.log(e.target.value);
+    datos[e.target.id] = e.target.value;   
+}
+ 
+// muestra envio correcto
+function mostrarMensaje (mensaje) {
+    const alerta = document.createElement('P');
+    alerta.textContent = mensaje;
+    alerta.classList.add('correcto');
+
+    formulario.appendChild(alerta);
+
+    // desaparezca despues de 5 segundos
+    setTimeout(() => {
+        alerta.remove();
+    }, 2000)
 }
 
-document.addEventListener('DOMContentLoaded', function () { // solo espera el hmtl, pero no espera css ni imagenes
-    console.log(4);
-})
+//Muestra un erros en pantalla 
+function mostrarError(mensaje) {
+    const error = document.createElement('P');
+    error.textContent = mensaje;
+    error.classList.add('error');
 
-console.log(5);
+    formulario.appendChild(error);
 
-window.onscroll = function () {
-    console.log('scrolling....');
+    // desaparezca despues de 5 segundos
+    setTimeout(() => {
+        error.remove();
+    }, 2000)
 }
+
+
+
+
+   
